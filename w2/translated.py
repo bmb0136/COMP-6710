106859d33761a8c9e5336b44a138dd443eb18bb8
Brandon Buckley <brandonmb0136@gmail.com>
2026-09-15 19:38:39 -0500

    w2: translate and run programs

--- w2/translated.py
+++ w2/translated.py
@@ -1,1 +1,20 @@
-# todo
+import sys
+
+if __name__ == '__main__':
+  try:
+    file = open("data.csv", "r")
+  except OSError:
+    print("Unable to open file", file=sys.stderr)
+    sys.exit(1)
+
+  for line in file:
+    line = line.rstrip('\n')
+    tokens = line.split(',')
+    column = 0
+
+    for token in tokens:
+      print(f"Row {column // 3}, Col {column % 3}: {token}")
+      column += 1
+
+  file.close()
+  sys.exit(0)
